e6493fa94c6a2328c5c7fe9bf58ad996b68e5128
dmehra2102 <deepanshumehra2102@gmail.com>
2024-09-12 23:00:55 +0530

    feat : added test case for the blog feaure and auth

---
+++ tests/blogs.test.js
@@ -0,0 +1,74 @@
+const Page = require("./helpers/page");
+
+let page;
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto("localhost:3000");
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+describe("When logged in", async () => {
+  beforeEach(async () => {
+    await page.login();
+    await page.click("a.btn-floating");
+  });
+
+  test("can see blog create form", async () => {
+    const label = await page.getContentsOf("form label");
+    expect(label).toEqual("Blog Title");
+  });
+
+  describe("And using valid inputs", async () => {
+    beforeEach(async () => {
+      await page.type(".title input", "My Title");
+      await page.type(".content input", "My Content");
+      await page.click("form button");
+    });
+
+    test("Submitting takes user to review screen", async () => {
+      const text = await page.getContentsOf("h5");
+      expect(text).toEqual("Please confirm your entries");
+    });
+
+    test("submitting then saving add blogs to index page", async () => {
+      await page.click("button.green");
+      await page.waitFor(".card");
+
+      const title = await page.getContentsOf(".card-title");
+      const content = await page.getContentsOf("p");
+
+      expect(title).toEqual("My Title");
+      expect(content).toEqual("My Content");
+    });
+  });
+
+  describe("And user put invalid input in form", async () => {
+    beforeEach(async () => {
+      await page.click("form button");
+    });
+
+    test("the form shows an error message", async () => {
+      const titleError = await page.getContentsOf(".title .red-text");
+      const contentError = await page.getContentsOf(".content .red-text");
+
+      expect(titleError).toEqual("You must provide a value");
+      expect(contentError).toEqual("You must provide a value");
+    });
+  });
+});
+
+describe("when user not logged in", async () => {
+  test("user should not been able to create blog", async () => {
+    const result = await page.post("/api/blogs", { title: "My Title", content: "My Content" });
+    expect(result).toEqual({ error: "You must log in!" });
+  });
+
+  test("user cannot get a list of blogs", async () => {
+    const result = await page.get("/api/blogs");
+    expect(result).toEqual({ error: "You must log in!" });
+  });
+});
---
+++ tests/factories/sessionFactory.js
@@ -0,0 +1,17 @@
+const Keygrip = require("keygrip");
+const keys = require("../../config/keys");
+const Buffer = require("safe-buffer").Buffer;
+
+const keygrip = new Keygrip([keys.cookieKey]);
+
+module.exports = (user) => {
+  const sessionObject = {
+    passport: {
+      user: user._id.toString(),
+    },
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString("base64");
+  const sig = keygrip.sign("session=" + sessionString);
+
+  return { session: sessionString, sig };
+};
